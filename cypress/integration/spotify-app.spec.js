6b0b2ee8f021cd41587d399e44ff23d0c6d868fd
mervearas <mervegurzel@icloud.com>
2021-03-25 16:06:00 +0100

    removed the console.log"

--- cypress/integration/spotify-app.spec.js
+++ cypress/integration/spotify-app.spec.js
@@ -7,9 +7,7 @@ context("check the functionalities of the spotify app", () => {
     })
     describe('verify the API', () => {
         it('it should access the spotify API and validates the top track list',() =>{
-            cy.intercept('https://api.spotify.com/v1/me/player', {fixture: "player.json"})
             cy.wait('@player').then(res => {
-                console.log(res)
                 cy.get('ul > :nth-child(1)')
                 .should('have.text', 'Anyone')
             })
