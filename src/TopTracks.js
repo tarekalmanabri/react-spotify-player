830ed7009f0f09dec89b52b854b5b8b58d64fe4d
Saoud <saoud.b94@gmail.com>
2021-03-25 15:56:14 +0100

    Destructuring props

--- src/TopTracks.js
+++ src/TopTracks.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { getTopTracks } from './getTopTracks'
 
-const TopTracks = (props) => {
+const TopTracks = ({ item, token }) => {
   const [topTracks, setTopTracks] = useState()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
 
   useEffect(() => {
     setLoading(true)
-    getTopTracks(props.item.artists[0].id, props.token)
+    getTopTracks(item.artists[0].id, token)
       .then((data) => {
         setTopTracks(data)
         setLoading(false)
@@ -17,12 +17,12 @@ const TopTracks = (props) => {
         setError(error)
         setLoading(false)
       })
-  }, [props.item])
+  }, [item])
 
   return (
     <>
       <div className='top-track-list'>
-        <h2>Top Track for {props.item.artists[0].name}</h2>
+        <h2>Top Track for {item.artists[0].name}</h2>
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
         <ul>{topTracks && topTracks.map((track) => <li>{track.name}</li>)}</ul>
