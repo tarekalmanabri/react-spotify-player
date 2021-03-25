621be0dac5cdaf4a7a6ab5cbb8dc5943131699a0
mervearas <mervegurzel@icloud.com>
2021-03-25 15:57:25 +0100

    addded fixture

--- cypress/integration/spotify-app.spec.js
+++ cypress/integration/spotify-app.spec.js
@@ -2,11 +2,17 @@
 
 context("check the functionalities of the spotify app", () => {
     beforeEach(() => {
-        cy.login('','')
+        cy.login('mervearas435@gmail.com','emir123.')
+        cy.intercept('https://api.spotify.com/v1/me/player', {fixture: "player.json"}).as('player')
     })
-    describe('', () => {
-        it('',() =>{
-            
+    describe('verify the API', () => {
+        it('it should access the spotify API and validates the top track list',() =>{
+            cy.intercept('https://api.spotify.com/v1/me/player', {fixture: "player.json"})
+            cy.wait('@player').then(res => {
+                console.log(res)
+                cy.get('ul > :nth-child(1)')
+                .should('have.text', 'Anyone')
+            })
         })
        
     })
