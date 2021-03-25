78e9157b9f10d7ff0176180ac6b237e19ed5702c
Saoud <saoud.b94@gmail.com>
2021-03-25 15:52:58 +0100

    add loading state

--- src/TopTracks.js
+++ src/TopTracks.js
@@ -3,15 +3,19 @@ import { getTopTracks } from './getTopTracks'
 
 const TopTracks = (props) => {
   const [topTracks, setTopTracks] = useState()
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
 
   useEffect(() => {
+    setLoading(true)
     getTopTracks(props.item.artists[0].id, props.token)
       .then((data) => {
         setTopTracks(data)
+        setLoading(false)
       })
       .catch((error) => {
         setError(error)
+        setLoading(false)
       })
   }, [props.item])
 
@@ -19,6 +23,7 @@ const TopTracks = (props) => {
     <>
       <div className='top-track-list'>
         <h2>Top Track for {props.item.artists[0].name}</h2>
+        {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
         <ul>{topTracks && topTracks.map((track) => <li>{track.name}</li>)}</ul>
       </div>
